4e1c085d1bfb0151a035d54372e87a67fefabe16
Rozana <rozanafirdausi12@gmail.com>
2018-04-08 23:45:02 +0700

    membuat database
    bikin migrate an

---
+++ database/migrations/2018_04_08_155315_create_table_barang.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableBarang extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama_barang',100);
+            $table->integer('harga_barang');
+            $table->integer('harga_jual');
+            $table->integer('stok');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('barang');
+    }
+}
---
+++ database/migrations/2018_04_08_163306_create_table_desa_binaan.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableDesaBinaan extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('desa_binaan', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama_desa_binaan',50);
+            $table->string('alamat_desa_binaan',100);
+            $table->string('contact_person',50);
+            $table->string('telp_contact_person',13);
+            $table->integer('jumlah_warga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('desa_binaan');
+    }
+}
