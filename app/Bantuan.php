2b0ceef7f450fc4085ea2ed353da6831a694a047
Atul <atulmunawaroh77@gmail.com>
2018-05-18 14:15:44 +0700

    revisi model, tampilan penjualan

--- app/Bantuan.php
+++ app/Bantuan.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Bantuan extends Model
 {
     //
+    protected $table = 'transaksi_bantuan';
+    
+    protected $fillable = [
+    	'id_admin',
+    	'nama_donatur',
+    	'jenis_bantuan',
+    	'jumlah_bantuan'
+    ];
 }
--- app/Penjualan.php
+++ app/Penjualan.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Penjualan extends Model
 {
     //
+    protected $table = 'transaksi_penjualan';
+    
+    protected $fillable = [
+    	'id_admin',
+    	'jumlah_terjual'
+    ];
 }
