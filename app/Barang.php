45ba4c6b60301e42c1a8c9efb179d025a0f8f269
Atul <atulmunawaroh77@gmail.com>
2018-05-19 09:43:15 +0700

    delete barang-masih error

--- app/Barang.php
+++ app/Barang.php
@@ -7,8 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Barang extends Model
 {
     //
+
     protected $table = 'barang';
-    
+    protected $primaryKey = 'id_barang';
+        
     protected $fillable = [
     	'nama_barang',
     	'harga_barang',
--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -11,6 +11,7 @@ class BarangController extends Controller
     public function index()
     {
     	$data = Barang::all();
+    	// dd($data);
     	return view('barang.barang', compact('data'));
     } 
 
@@ -20,8 +21,27 @@ class BarangController extends Controller
     	return redirect('barang');
     }
 
-    public function edit(Request $request)
+    public function edit($id)
     {
-    	
+    	$data = Barang::findOrFail($id);
+    	return view('barang.barang', compact('data'));
+    }
+
+    public function update(Request $request, $id)
+    {
+    	$data = Barang::where('id_barang',$id)->first();  	
+    	$data->nama_barang = $request->nama_barang;
+    	$data->harga_barang = $request->harga_barang;
+    	$data->harga_jual = $request->harga_jual;
+    	$data->stok = $request->stok;
+    	$data->save();
+    	return redirect('barang');
+
+    }
+
+    public function destroy(Request $request, $id)
+    {
+    	$data = Barang::where('id_barang',$id)->delete();
+    	return back();
     }
 }
